0ea03878f229d9cefa24c18db886b87ae0de9f95
Shim su yeon <84979352+Shimsuyeon@users.noreply.github.com>
2024-04-30 14:20:12 +0900

    detail

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,11 +1,26 @@
+import "./main.css";
+
+import { useNavigate } from "react-router-dom";
+import Button from "src/atoms/button/Button";
 import Area from "src/atoms/containers/area/Area";
 import Content from "src/atoms/containers/content/Content";
-
+import Text from "src/atoms/text/Text";
 const MainPage = () => {
+  const navigate = useNavigate();
+  const Book = () => {
+    navigate("/books");
+  };
   return (
     <>
       <Area backgroundColor={"#797aab"}>
-        <Content></Content>
+        <Content>
+          <div className="bookshelf">
+            <div className="books"></div>
+          </div>
+          <Button onClick={Book}>
+            <Text>책장으로</Text>
+          </Button>
+        </Content>
       </Area>
     </>
   );
